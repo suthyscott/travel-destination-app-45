673b77cba8282789662ef932cc8f5a8251a20165
suthyscott <sutherlandscott21@gmail.com>
2024-04-02 15:43:38 -0600

    styling header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react"
+import { NavLink } from "react-router-dom"
 
 const Header = () => {
-  return (
-    <div className='flex justify-center items-center h-[10vh] w-full bg-slate-500'>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='addDestination'>Add Destination</NavLink>
-    </div>
-  )
+    return (
+        <div className="flex justify-between items-center h-[10vh] w-full bg-slate-500 px-16 text-white">
+            <h1 className="text-2xl">Welcome to the Travel Destination App!</h1>
+            <div className="w-1/4 flex justify-evenly">
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => {
+                        console.log(isActive)
+                        return isActive ? "underline" : ""
+                    }}
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                    to="addDestination"
+                    className={({ isActive }) => {
+                        console.log(isActive)
+                        return isActive ? "underline" : ""
+                    }}
+                >
+                    Add Destination
+                </NavLink>
+            </div>
+        </div>
+    )
 }
 
 export default Header
